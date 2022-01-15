20dfdf1fe68c104f34f77399348fb048a0af0135
IgorVini25 <igvinps2006@gmail.com>
2022-01-15 00:00:12 -0300

    :sparkles: Create Users Table

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,7 @@
+import { Entity, EntityRepository, Repository } from 'typeorm'
+import { User } from '../entities/User'
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {}
+
+export { UsersRepository }
