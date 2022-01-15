40e485d2f3f6f76ba11d07ca89c8ecdd388f41d9
IgorVini25 <igvinps2006@gmail.com>
2022-01-14 23:31:09 -0300

    :tada: Server Config

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata'
+import express from 'express'
+import 'express-async-errors'
+import { errors } from './middlewares/errors'
+
+import './database'
+import { router } from './routes'
+
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(router)
+
+app.use(errors)
+
+app.listen(3333, () => console.log('Server running at http://localhost:3333'))
