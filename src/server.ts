d41e573778163215fce4a3755fab2c73c7e95c7d
IgorVini25 <igvinps2006@gmail.com>
2022-01-26 21:43:02 -0300

    Config to Front End

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata'
 import express from 'express'
 import 'express-async-errors'
 import { errors } from './middlewares/errors'
+import path from 'path'
+import ejs from 'ejs'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -16,6 +18,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(router)
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+app.set('views', path.join(__dirname, '..', 'public'))
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
 
 app.use(errors)
 
